d66af925e1ad70fe9db0cdf36ba15d650b026ee1
Hasan Alp Zengin <hasalp38@gmail.com>
2020-05-10 10:47:49 +0200

    heroku initilization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,114 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'langsyntaxeditor';
+
+  public text: String = "";
+  public result: String = "";
+  public isTurkish: boolean = false;
+  public filteredWords: String = "";
+  private filterList: String[] = [];
+
+  uppercaseall(){
+    let result = "";
+    let words = this.text.split(" ");
+    words.forEach(word=>{
+      if(this.isInFilter(word.replace(/\s{2,}/g,""))){
+        result += word.trim()+" ";
+      }else{
+        if(this.isTurkish){
+          result += word.toLocaleUpperCase("tr-TR").trim()+" ";
+        }else{
+          result += word.toUpperCase().trim()+" ";
+        }
+      }
+    });
+    this.result = result.slice(0, result.length-1);
+  }
+
+  lowercaseall(){
+    let result = "";
+    let words = this.text.split(" ");
+    words.forEach(word=>{
+      if(this.isInFilter(word.replace(/\s{2,}/g,""))){
+        result += word.trim()+" ";
+      }else{
+        if(this.isTurkish){
+          result += word.toLocaleLowerCase("tr-TR").trim()+" ";
+        }else{
+          result += word.toLowerCase().trim()+" ";
+        }
+      }
+    });
+    this.result = result.slice(0, result.length-1);
+  }
+
+  capitilizesentences(){
+    let that = this;
+    if(this.isTurkish){
+      this.result = this.text.replace(/.+?[\.\?\!](\s|$)/g, function (txt) {
+        if(that.isInFilter(txt)){
+          return txt;
+        }
+        return txt.charAt(0).toLocaleUpperCase("tr-TR") + txt.substr(1).toLocaleLowerCase("tr-TR");
+      });
+    }else{
+      this.result = this.text.replace(/.+?[\.\?\!](\s|$)/g, function (txt) {
+        if(that.isInFilter(txt)){
+          return txt;
+        }
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      });
+    }
+  }
+
+  capitilizewords(){
+    let resultWords: string[] = [];
+    let words = this.text.split(" ");
+    words.forEach(word => {
+      if(!this.isInFilter(word)){
+        if(this.isTurkish){
+          resultWords.push(word.charAt(0).toLocaleUpperCase("tr-TR")+word.substr(1).toLocaleLowerCase("tr-TR"));
+        }else{
+          resultWords.push(word.charAt(0).toUpperCase()+word.substr(1).toLowerCase());
+        }
+      }else{
+        resultWords.push(word);
+      }
+    });
+    this.result = this.wordListToSingleString(resultWords);
+  }
+
+  copyResult(resultArea){
+    resultArea.select();
+    document.execCommand('copy');
+    resultArea.setSelectionRange(0, 0);
+  }
+
+  filterChanged(event: string){
+    //clean filter
+    this.filterList.splice(0);
+    event.split(",").forEach(word=>{
+      this.filterList.push(word.trim());
+    });
+  }
+
+  public isInFilter(word: string){
+    //if sentence
+    word = word.split(" ")[0];
+    for(var filter in this.filterList){
+      if(this.filterList[filter].toLowerCase().localeCompare(word.toLowerCase())===0){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private wordListToSingleString(wordlist:string[]){
+    let result = "";
+    wordlist.forEach(word=>{
+      result += word+" ";
+    });
+    return result.trim();
+  }
+
 }
